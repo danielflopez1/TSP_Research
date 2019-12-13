5e4ba191b105650b5f14d083b03b8136b75496f8
danielflopez1 <danielflopez1@gmail.com>
2019-12-13 03:23:50 -0600

    Update DataGenerator.py

--- DataGenerator.py
+++ DataGenerator.py
@@ -6,31 +6,39 @@ import time
 
 
 class Data_Generator:
-    def __init__(self):  # set initial variables for data generation
-        self.change = 0
-        self.size = 5 
+    def __init__(self, size, num_data, directed = True):  # set initial variables for data generation
+        self.size = size 
         self.max_value = 100  
+        self.num_data = num_values
 
     def tsp_data(self):
         xs = []
         ys = []
-        for x in range(self.change,1000000):  #Generate 1M datapoints
+        yps = []
+        for x in range(self.change,self.num_data):  #Generate 1M datapoints
             GG = GraphGenerator.GraphGenerator(self.size, self.max_value)
-            GG.undirected_graph()
+            if directed:
+                GG.directed_graph() #check if adjacency matrix is directed or undirected
+            else:
+                GG.undirected_graph()
             g = np.array(GG.get_adjacency_matrix())  #Create undirected adjacency matrix
             matrx = g.copy()
             g[g==0] = 999
-            self.path = tsp.tsp_dp_solve(g)  #solve the tsp
-            self.tsp_val = tsp.tour_len(self.path, g) #get the tsp cost
-            ys.append(self.tsp_val)             #Add the data to the arrays
+            path = tsp.tsp_dp_solve(g)  #solve the tsp
+            tsp_val = tsp.tour_len(self.path, g) #get the tsp cost
+            yps.append(path)
+            ys.append(tsp_val)             #Add the data to the arrays
             xs.append(list(matrx.flatten()))
-            self.change +=1
             
-        pickle_file=open('m5tspxs.pickle','wb') #Pickle the data for later use
+        pickle_file=open('xs.pickle','wb') #Pickle the data for later use
         pickle.dump(xs,pickle_file)
         pickle_file.close()
-
-        pickle_file= open('m5tspys.pickle', 'wb')
+        
+        pickle_file= open('yps.pickle', 'wb')
+        pickle.dump(ys,pickle_file)
+        pickle_file.close()
+        
+        pickle_file= open('ys.pickle', 'wb')
         pickle.dump(ys,pickle_file)
         pickle_file.close()
 
@@ -40,10 +48,13 @@ class Data_Generator:
         pickle_file.close()
         print(datas)
 
+        pickle_file = open('yps.pickle', 'rb')
+        ypathdatas = pickle.load(pickle_file)
+        pickle_file.close()
+        print(ypathdatas)
+        
         pickle_file = open('ys.pickle', 'rb')
         ydatas = pickle.load(pickle_file)
         pickle_file.close()
         print("---",ydatas)
-gm = Data_Generator()
-gm.tsp_data()
-gm.get_pickles()
+
